bea151d39c6b14d4bf5e76e249a58ed294bf62c1
Ch3tra <Chetra.ou@yahoo.com>
2023-11-20 03:45:37 +0700

    mysql

--- database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine, text
-
-# Connect to the database
-engine = create_engine("mysql+mysqlconnector://root:@localhost/ss34_proo")
-# Test the connection
-connection = engine.connect()
-
--- route/apiProduct.py
+++ route/apiProduct.py
@@ -1,28 +1,3 @@
-# from flask import Blueprint
-# from database import connection, text
-#
-# apiD = Blueprint('apiD', __name__)
-#
-#
-# @apiD.route('/getAllProduct')
-# def getAllProduct():
-#     products = connection.execute(text('select * from pproduct'))
-#     connection.commit()
-#
-#     json_string = []
-#     for product in products:
-#         json_string.append(
-#             {
-#                 'id': product.id,
-#                 'name': product.name,
-#                 'discount': product.discount,
-#                 'price': product.price,
-#                 'image': product.image,
-#             }
-#         )
-#     return json_string
-
-
 from flask import Blueprint
 from config import execute_query
 
