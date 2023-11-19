c496b043f9cf6a0d9ab2c1625f1cf52a0aa9f805
Ch3tra <Chetra.ou@yahoo.com>
2023-11-20 03:34:10 +0700

    api

--- route/apiProduct.py
+++ route/apiProduct.py
@@ -1,25 +1,50 @@
+# from flask import Blueprint
+# from database import connection, text
+#
+# apiD = Blueprint('apiD', __name__)
+#
+#
+# @apiD.route('/getAllProduct')
+# def getAllProduct():
+#     products = connection.execute(text('select * from pproduct'))
+#     connection.commit()
+#
+#     json_string = []
+#     for product in products:
+#         json_string.append(
+#             {
+#                 'id': product.id,
+#                 'name': product.name,
+#                 'discount': product.discount,
+#                 'price': product.price,
+#                 'image': product.image,
+#             }
+#         )
+#     return json_string
+
+
 from flask import Blueprint
-from database import connection, text
+from config import execute_query
 
 apiD = Blueprint('apiD', __name__)
 
-
 @apiD.route('/getAllProduct')
 def getAllProduct():
-    products = connection.execute(text('select * from pproduct'))
-    connection.commit()
+    query = "SELECT * FROM pproduct"
+    products = execute_query(query)
 
     json_string = []
     for product in products:
         json_string.append(
             {
-                'id': product.id,
-                'name': product.name,
-                'discount': product.discount,
-                'price': product.price,
-                'image': product.image,
+                'id': product['id'],
+                'name': product['name'],
+                'discount': product['discount'],
+                'price': product['price'],
+                'image': product['image'],
             }
         )
     return json_string
+
 
 
